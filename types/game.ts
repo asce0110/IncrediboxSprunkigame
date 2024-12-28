c6f5bda4c69856a1dba290c6f5258c44dc532f40
asce <403153162@qq.com>
2024-12-28 20:15:02 +0800

    gt

--- types/game.ts
+++ types/game.ts
@@ -1,12 +1,10 @@
-import { Database } from './supabase'
-
-export type Game = Database['public']['Tables']['games']['Row']
-
-export interface GameFormData {
-  title: string
-  description: string
-  coverImage: string
-  categoryId: string
-  url: string
-  featured: boolean
+export interface Game {
+  id: number;
+  created_at: string;
+  title: string;
+  description: string | null;
+  image_url: string | null;
+  category_id: number;
+  itch_game_id: string;
+  slug: string;
 }
